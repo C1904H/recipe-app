dc5935a6bbca40799f2e5b9fdb97dcad406159bd
C1904H <164913030+C1904H@users.noreply.github.com>
2025-01-08 20:34:05 +0000

    updated protected views

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -6,10 +6,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 def home(request):
   return render(request, 'recipes/recipes_home.html')
 
-class RecipeListView(ListView, LoginRequiredMixin):
+class RecipeListView(LoginRequiredMixin, ListView):
   model = Recipe
   template_name = 'recipes/recipe_list.html'
 
-class RecipeDetailView(DetailView, LoginRequiredMixin):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
   model = Recipe
   template_name = 'recipes/recipe_detail.html'
